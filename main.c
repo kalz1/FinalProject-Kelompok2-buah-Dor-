f17f88406da1232047871e2a8317cd051b7ddf24
kalz1 <haikalinzaghi18@gmail.com>
2025-06-05 21:40:34 +0700

    initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct PLTS_Data {
+    int tahun;
+    float energiHarian;
+    float totalEnergi;
+    int lokasi; // 1: Rumah, 2: Gedung, 3: Lahan
+};
+
+int main(){
+
+    return 0;
+}
